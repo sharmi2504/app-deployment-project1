a54e6dc621f57693e62de679037fc706a57e88b6
sharmi2504 <sharmilasivasubramanian@gmail.com>
2024-08-16 23:21:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+if (env.BRANCH_NAME == 'dev') {
 echo "Starting new containers..."
 docker run -d --name nginx_container -p 80:80
 docker-compose up -d
 docker tag nginximage sharmi2504:dev
 docker push sharmi2504:dev
+
+} else {
+         echo "Skipping deploy for branch other than dev"
+                    }
 
