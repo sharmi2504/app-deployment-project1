99d4bb437df207fed410f2b4116f3e00669a7d01
sharmi2504 <sharmilasivasubramanian@gmail.com>
2024-08-16 19:34:46 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Starting new containers..."
+docker run -d --name nginx_container -p 80:80
+docker-compose up -d
 docker tag nginximage sharmi2504:dev
 docker push sharmi2504:dev
 
