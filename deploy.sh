77d87e76a26dc9566ea161d134b7f90d0bf7a51a
sharmi2504 <sharmilasivasubramanian@gmail.com>
2024-08-17 09:38:53 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,21 @@
 
 # Start new containers
 echo "Starting new containers..."
-docker run -d --name nginx_container -p 80:80 nginx:latest  # Specify the image name here
+docker run -d --name nginxcontainer -p 80:80 nginx:latest
 
-# Assuming docker-compose.yml is properly set up
+# Assuming docker-compose.yml is properly set up or not
 docker-compose up -d
 
 # Tag and push Docker image
 echo "Tagging and pushing Docker image..."
-docker tag nginx:latest sharmi2504/nginx:dev  # Assuming 'nginx:latest' is your source image
-docker push sharmi2504/nginx:dev  # Specify the complete repository name
+docker tag nginx:latest sharmi2504/nginx:dev  
+docker push sharmi2504/nginx:dev  
 
 # Deployment based on branch name
 if [ "$BRANCH_NAME" = "dev" ]; then
   echo "Deploying to development environment"
-  # Your deployment commands here
-else
-  echo "Not in the development branch. Skipping deployment."
+  else
+  echo "Not in the dev branch. Skipping deployment."
 fi
 
 
