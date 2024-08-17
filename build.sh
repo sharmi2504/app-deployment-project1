c0f159e84d7c68a59a236375b2d4e43662f94669
sharmi2504 <sharmilasivasubramanian@gmail.com>
2024-08-17 11:48:30 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-# build the docker image [nginxiamge] 
-docker build -t nginximage .
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u sharmi2504 -p $DOCKER_PASSWORD
+    docker tag projectimage sharmi2504/dev
+    docker push sharmi2504/dev
+
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u sharmi2504 -p $DOCKER_PASSWORD
+    docker tag projectimage sharmi2504/prod 
+    docker push sharmi2504/prod
+fi
