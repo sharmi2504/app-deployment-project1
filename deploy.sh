8455ec8c16b7be01a626dba0eae7f9f965e4f1f0
sharmi2504 <sharmilasivasubramanian@gmail.com>
2024-08-19 11:24:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 docker login -u sharmi2504 -p dckr_pat_9SQ5F6VWmpfq_5dLNHHeqxN2XZI
-if [ $GIT_BRANCH = "origin/dev" ]; then
+if [ $GIT_BRANCH = "dev" ]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker tag projectimage sharmi2504/dev
     docker push sharmi2504/dev
 
-elif [ $GIT_BRANCH = "origin/master" ]; then
+elif [ $GIT_BRANCH = "master" ]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker tag projectimage sharmi2504/prod 
